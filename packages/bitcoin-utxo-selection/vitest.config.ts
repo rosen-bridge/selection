8da70247f4e6f9d063e3a0cae1083b9cb4c90841
rasoul salehi <8raha6094@gmail.com>
2026-02-16 15:12:32 +0330

    update vitest config in each packages

--- packages/bitcoin-utxo-selection/vitest.config.ts
+++ packages/bitcoin-utxo-selection/vitest.config.ts
@@ -1,12 +1,5 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
+import { defineProject, mergeConfig } from 'vitest/config';
 
 import configShared from '../../vitest.shared';
 
-const projectSpecific = defineConfig({
-  test: {
-    coverage: {
-      include: ['src'],
-    },
-  },
-});
-export default mergeConfig(configShared, projectSpecific);
+export default mergeConfig(configShared, defineProject({}));
